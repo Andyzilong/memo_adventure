3d82985cec312fed3eead49862561b90d756c5fe
Andy Xie <andyxie@mac.speedport.ip>
2025-06-11 11:33:08 +0200

    Level01 backgroudn done

--- lib/memo_adventure.dart
+++ lib/memo_adventure.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:memo_adventure/levels/level.dart';
 
 class MemoAdventure extends FlameGame {
-
+  @override
+  Color backgroundColor() => const Color(0xFf211F30);
   late final CameraComponent cam;
   
   final world = Level();
@@ -15,6 +17,7 @@ class MemoAdventure extends FlameGame {
     cam = CameraComponent.withFixedResolution(
       world: world, width: 640, height: 360
     );
+    cam.viewfinder.anchor = Anchor.topLeft;
     
     addAll([cam, world]);
     return super.onLoad();
